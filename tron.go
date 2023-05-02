8cb3d57f0ca0c8bc9a6d731c1ac7cee5d7bd699a
iqquee <hisyntax0@gmail.com>
2023-05-02 19:38:28 +0100

    created NewRequest method to handle all http requests

--- tron.go
+++ tron.go
@@ -1,6 +1,17 @@
 package tron
 
-type Tron struct{}
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+type Client struct {
+	Http   *http.Client
+	ApiKey string
+}
+
 
 const (
 	MainNet       = "https://api.trongrid.io"
@@ -8,6 +19,38 @@ const (
 	NileTestNet   = "https://nile.trongrid.io"
 )
 
-func New() *Tron {
-	return &Tron{}
+func New(apiKey string) *Client {
+	return &Client{
+		ApiKey: apiKey,
+	}
+}
+
+
+func (c *Client) NewRequest(method, url string, isPayload bool, payload interface{}) ([]byte, int, error) {
+	var newPayload []byte
+	if isPayload {
+		jsonReq, jsonReqErr := json.Marshal(&payload)
+		if jsonReqErr != nil {
+			return nil, 0, jsonReqErr
+		}
+		newPayload = jsonReq
+	}
+
+	req, reqErr := http.NewRequest(method, url, bytes.NewBuffer(newPayload))
+	if reqErr != nil {
+		return nil, 0, reqErr
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("TRON-PRO-API-KEY", c.ApiKey)
+
+	resp, respErr := c.Http.Do(req)
+	if respErr != nil {
+		return nil, 0, respErr
+	}
+
+	defer resp.Body.Close()
+	resp_body, _ := ioutil.ReadAll(resp.Body)
+
+	return resp_body, resp.StatusCode, nil
 }
